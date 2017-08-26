120bdeab9eaf3999b1d121c1f2241719ac4c37dd
Claudia El Khatib <claudiacv22@gmail.com>
2017-08-26 17:11:25 -0400

    Successfully tested CRUD for the diary resource

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,3 +1,4 @@
 class Challenge < ApplicationRecord
   belongs_to :user
+  has_many :diaries
 end
--- db/migrate/20170825202722_create_diaries.rb
+++ db/migrate/20170825202722_create_diaries.rb
@@ -1,6 +1,7 @@
 class CreateDiaries < ActiveRecord::Migration[5.0]
   def change
     create_table :diaries do |t|
+      t.belongs_to :challenge, index: true
       t.date :day
       t.string :description
       t.boolean :status
